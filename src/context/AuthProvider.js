c52666b1dbc8708a340b9e1922c5e261c541caff
dzennank <dzenankrlic@gmail.com>
2022-05-12 13:57:18 +0200

    create auth context and state

--- src/context/AuthProvider.js
+++ src/context/AuthProvider.js
@@ -1,15 +1,15 @@
 import { createContext, useState } from "react";
 
-const AuthContext = createContext({}) ;
+const AuthContext = createContext({});
 
-export const AuthProvider = ({ childern }) => {
+export const AuthProvider = ({ children }) => {
     const [auth, setAuth] = useState({});
 
-
     return (
-        <AuthContext.Provider value={{auth, setAuth}}>
-            {childern}
+        <AuthContext.Provider value={{ auth, setAuth }}>
+            {children}
         </AuthContext.Provider>
     )
 }
+
 export default AuthContext;
